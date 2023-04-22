99c6e76e6fde6ed483e39ad11e0e5a3783846a53
Krezcs <130219080+Krezcs@users.noreply.github.com>
2023-04-22 18:05:41 +0300

    Update hw8.py

--- hw8.py
+++ hw8.py
@@ -1,16 +1,20 @@
 import datetime
 
-def get_birthdays_per_week(users):
+def get_birthdays(users):
     weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-    birthdays_per_week = {weekday: [] for weekday in weekdays}
+    birthdays = {weekday: [] for weekday in weekdays}
+    today = datetime.date.today()
+    if today.weekday() == 0:  
+        today = today - datetime.timedelta(days=2)
     for user in users:
         birthday = user['birthday']
-        if birthday.weekday() >= 5:  
+        if birthday.weekday() in [5, 6]:  
             birthday = birthday + datetime.timedelta(days=7 - birthday.weekday())  
+        if today.weekday() == 0 and birthday.weekday() in [5, 6]:  
+            birthday = birthday - datetime.timedelta(days=2) 
         birthday_day_of_week = birthday.strftime('%A')
-        birthdays_per_week[birthday_day_of_week].append(user)
-    return birthdays_per_week
-
+        birthdays[birthday_day_of_week].append(user)
+    return birthdays
 
 users = [
     {'name': 'John', 'birthday': datetime.date(1995, 1, 15)},
@@ -26,14 +30,21 @@ users = [
     {'name': 'James', 'birthday': datetime.date(1986, 8, 14)},
     {'name': 'Emma', 'birthday': datetime.date(1994, 5, 6)},
     {'name': 'Daniel', 'birthday': datetime.date(1991, 12, 25)},
+    {'name': 'Sophie', 'birthday': datetime.date(1999, 9, 20)},
+    {'name': 'Jacob', 'birthday': datetime.date(1996, 6, 18)},
+    {'name': 'Grace', 'birthday': datetime.date(1989, 4, 5)},
+    {'name': 'William', 'birthday': datetime.date(1992, 1, 30)},
+    {'name': 'Lily', 'birthday': datetime.date(1997, 8, 7)},
+    {'name': 'Ryan', 'birthday': datetime.date(1987, 11, 10)},
+    {'name': 'Ava', 'birthday': datetime.date(1993, 3, 12)},
+    {'name': 'Ethan', 'birthday': datetime.date(1995, 10, 1)},
+    {'name': 'Mia', 'birthday': datetime.date(1998, 7, 22)},
+    {'name': 'Andrew', 'birthday': datetime.date(1990, 2, 15)},
 ]
 
-birthdays = get_birthdays_per_week(users)
+birthdays = get_birthdays(users)
 for weekday, users in birthdays.items():
     print(f"{weekday}: {', '.join([user['name'] for user in users])}")
 
-print('Saturday : should be greeted on Monday')
-print('Sunday : should be greeted on Monday')
-
-
-
+print('Saturday : should be greeted on next Monday')
+print('Sunday : should be greeted on next Monday')
